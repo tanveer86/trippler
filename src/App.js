02b1d9851b4014d7ce8fe68b0fa64d599e1352b9
Tanveer <tanveerk86@gmail.com>
2019-10-16 03:52:30 +0530

    Basic Routing For All Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import Homepage from './components/home/Homepage';
+import Category from './components/home/Category';
+import ProductDetail from './components/home/ProductDetail';
+import Booking from './components/home/Booking';
+import Sucess from './components/home/Sucess';
+import User from './components/user/User';
+import OrderList from './components/user/OrderList';
+import OrderDetail from './components/user/OrderDetail';
+import LiveBidding from './components/user/LiveBidding';
+import Register from './components/home/Register';
+import Login from './components/home/Login';
+import Payments from './components/vendor/Payments';
+import BiddingDetail from './components/vendor/BiddingDetail';
+import BiddingList from './components/vendor/BiddingList';
+import VendorOrderDetail from './components/vendor/OrderDetail';
+import VendorOrderList from './components/vendor/OrderList';
+import AddingImportProducts from './components/vendor/AddingImportProducts';
+import VendorRegistration from './components/home/VendorRegistration';
+import AdminAddProduct from './components/admin/AddProduct';
 
-function App(){
-    return(
-        <React.Fragment>
-            this is from app
-        </React.Fragment>
-    )
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Homepage} />
+                    <Route path="/category" exact component={Category} />
+                    <Route path="/category/product-detail" exact component={ProductDetail} />
+                    <Route path="/booking" exact component={Booking} />
+                    <Route path="/sucess" exact component={Sucess} />
+                    <Route path="/user" exact component={User} />
+                    <Route path="/user/order-list" exact component={OrderList} />
+                    <Route path="/user/order-detail" exact component={OrderDetail} />
+                    <Route path="/user/live-bidding" exact component={LiveBidding} />
+                    <Route path="/register" exact component={Register} />
+                    <Route path="/login" exact component={Login} />
+                    <Route path="//vendor/payments" exact component={Payments} />
+                    <Route path="/vendor/bidding-detail" exact component={BiddingDetail} />
+                    <Route path="/vendor/bidding-list" exact component={BiddingList} />
+                    <Route path="/vendor/order-detail" exact component={VendorOrderDetail} />
+                    <Route path="/vendor/order-list" exact component={VendorOrderList} />
+                    <Route path="/vendor/import-products" exact component={AddingImportProducts} />
+                    <Route path="/vendor-registration" exact component={VendorRegistration} />
+                    <Route path="/admin/add-product" exact component={AdminAddProduct} />
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
